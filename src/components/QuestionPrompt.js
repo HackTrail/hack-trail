45d2caae921195b170baffb6be4385c7ee873e1c
Juan Raigosa <juan.raigosa@opc-mbp-jraigosa.attlocal.net>
2022-06-27 16:47:09 -0500

    split out components in question

--- src/components/QuestionPrompt.js
+++ src/components/QuestionPrompt.js
@@ -1,12 +1,13 @@
 import React from "react";
+import { Question } from "./Question"
+import { ImageContainer } from "./ImageContainer"
 import './Prompt.css';
 
 const QuestionPrompt = ({question, image, options}) => {
     return (
         <div className="questionPrompt">
-            <div>{image}</div>
-            <div>{question}</div>
-            <div>{JSON.stringify(options)}</div>
+            <ImageContainer image={image}/>
+            <Question question={question} options={options}/>
         </div>
     );
 }
