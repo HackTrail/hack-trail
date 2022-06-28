4fcba85235690a940158df9cb7c3a42d3bffaccc
chethanagopinath <chethugopi12@gmail.com>
2022-06-28 13:32:13 -0500

    adding more styles and stuff

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,10 +1,17 @@
 import React from "react";
+import './Question.css';
 
 const Question = ({question, options}) => {
     return (
         <div>
             <div className="question">{question}</div>
-            <div className="options">{JSON.stringify(options)}</div>
+            <div className="options">
+                {
+                    options.map((each, index) => {
+                        return <button className="option">{index+1 + ". " + each.option}</button>
+                    })
+                }
+            </div>
         </div>
     );
 }
