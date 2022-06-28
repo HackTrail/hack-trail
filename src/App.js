954f74ce57a3c188a312da8aa3fa7dcfc1046aae
Megan Weijiang <megan.weijiang@gmail.com>
2022-06-28 17:48:09 -0500

    scoring logic

--- src/App.js
+++ src/App.js
@@ -13,6 +13,8 @@ function App() {
   const [isInStart, setIsInStart] = useState(true);
   const [questionsSeen, setQuestionsSeen] = useState(-1);
   const [isInInfo, setIsInInfo] = useState(false);
+  const [densityScore, setDensityScore] = useState(0);
+  const [infrastructureScore, setInfrastructureScore] = useState(0);
 
   const startGame = () => {
     clearChoicesRecord()
@@ -29,6 +31,11 @@ function App() {
     setIsInInfo(true);
   }
 
+  const updateScores = (densityEffect, infrastructureEffect) => {
+    setDensityScore(densityScore+densityEffect)
+    setInfrastructureScore(infrastructureScore+infrastructureEffect)
+  }
+
   const questions = require('./components/data/questionsData.json')
   const numQuestions = Object.keys(questions).length
 
@@ -37,15 +44,16 @@ function App() {
         return <Start startGame={startGame} image={startImage} questions={questions} />;
       }
       if (isInInfo) {   
-        return <Info nextQuestion={nextQuestion} text={"random text for now but this will come from the json"} />;
+        return <Info nextQuestion={nextQuestion} text={questions[questionsSeen].fact} />;
       }
       if (questionsSeen < numQuestions) { 
-        return <QuestionPrompt nextInfo={nextInfo} key={questions[questionsSeen].id} question={questions[questionsSeen].question} image={questions[questionsSeen].image} options={questions[questionsSeen].options} />;
+        return <QuestionPrompt nextInfo={nextInfo} updateScores={updateScores} id={questions[questionsSeen].id} text={questions[questionsSeen].question.text} image={questions[questionsSeen].question.image} options={questions[questionsSeen].question.options} />;
       }
       else {
         return <div>
           <h1>McMuffy's World</h1>
-              ggwp
+              ggwp <br></br>
+              density: {densityScore}, infrastructure: {infrastructureScore}
         </div>;
       }
     }
--- src/components/Question.js
+++ src/components/Question.js
@@ -1,16 +1,26 @@
 import React from "react";
 import './Question.css';
 import { v4 as uuidv4 } from 'uuid';
-// onSelect and onSubmit
+import { recordChoice } from '../utils/utils';
 
 const Question = (props) => {
+    const scoreAndContinue = (e) => {
+        recordChoice(props.id, e.target.id)
+        const choiceIndex = e.target.id - 1
+        const densityEffect = props.options[choiceIndex].densityEffect
+        const infrastructureEffect = props.options[choiceIndex].infrastructureEffect
+        props.updateScores(densityEffect, infrastructureEffect)
+
+        props.nextInfo()
+    }
+
     return (
         <div>
             <div className="question">{props.question}</div>
             <div className="options">
                 {
                     props.options.map((each, index) => {
-                        return (<button className="option" key={uuidv4()} onClick={props.nextInfo}>
+                        return (<button className="option" key={uuidv4()} id={each.choiceId} onClick={scoreAndContinue}>
                                 {index+1 + ". " + each.option}
                             </button>)
                     })
