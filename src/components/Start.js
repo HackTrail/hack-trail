75dbc3fd703fa6ad05b85b95c48efdba877b7625
chethanagopinath <chethugopi12@gmail.com>
2022-06-28 13:48:35 -0500

    add images and placeholder in data

--- src/components/Start.js
+++ src/components/Start.js
@@ -17,7 +17,7 @@ const Start = ({image, questions}) => {
             ) : (
                 <div className="wrapper">
                     <h1>McMuffy's World</h1>
-                    <ImageContainer className="center" alt="house with sun" image={image}/>
+                    <ImageContainer className="center" image={image}/>
                     <p>Pending an actual description. For now, I love cats and especially cats named Noodle! She's the best. Meow meow.</p>
                     <div className="centered-button-wrapper">
                         <button onClick={startGame}>Start</button>
