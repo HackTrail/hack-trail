9ef97143992e327e0cccb5de45c2b3518c57ff14
Michael Gardo <michael.gardo@opcity.com>
2022-06-28 14:52:12 -0500

    Add localStorage functions

---
+++ src/utils/utils.js
@@ -0,0 +1,26 @@
+
+export function getChoicesRecord(){
+	let choices = localStorage.getItem('choices');
+	if (choices) {
+	    return JSON.parse(choices);
+    } else {
+	    return null;
+    }
+}
+
+function setChoicesRecord(choices) {
+	localStorage.setItem('choices', JSON.stringify(choices));
+}
+
+export function clearChoicesRecord() {
+	localStorage.removeItem('choices');
+}
+
+export function recordChoice(questionId, choiceId) {
+    let choices = getChoicesRecord();
+    if (!choices) {
+        choices = []
+    }
+    choices.push({[questionId]: `${choiceId}`})
+    setChoicesRecord(choices)
+}
