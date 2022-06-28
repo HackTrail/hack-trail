2d8d31b19faa42f4a189599c0c760405dac8606d
Michael Gardo <michael.gardo@opcity.com>
2022-06-27 22:24:27 -0500

    Add question tally functions

--- src/db/firestore.js
+++ src/db/firestore.js
@@ -15,12 +15,47 @@ export const incrementOutcome = async (id) => {
     const outcomeRef = doc(db, "results", id);
     const outcomeSnapshot = await getDoc(outcomeRef);
     if (!outcomeSnapshot.exists()) {
-        console.log("Outcome doesn't exist")
+        console.log("Outcome doesn't exist");
         return;
     }
-    const currValue = outcomeSnapshot.data().value
+    const currValue = outcomeSnapshot.data().value;
 
     await updateDoc(outcomeRef, {
         value: currValue + 1
     });
+    return currValue;
 };
+
+async function questionRefAndSnapshot(questionId) {
+    const questionRef = doc(db, 'questions', questionId);
+    const questionSnapshot = await getDoc(questionRef);
+
+    return [questionRef, questionSnapshot];
+}
+
+export const getQuestionAnswerTally = async (questionId, answerId) => {
+    const [, questionSnapshot] = await questionRefAndSnapshot(questionId);
+    if (questionSnapshot.exists()) {
+        const data = questionSnapshot.data()
+        let totalAnswers = 0;
+        Object.keys(data).forEach(function(key,) {
+            totalAnswers += data[key]
+        })
+        return [questionSnapshot.data()[answerId], totalAnswers];
+    } else {
+        console.log("Question doesn't exist");
+    }
+}
+
+export const incrementQuestionAnswerTally = async (questionId, answerId) => {
+    const [questionRef, questionSnapshot] = await questionRefAndSnapshot(questionId);
+
+    if (questionSnapshot.exists()) {
+        let currValue = questionSnapshot.data()[answerId];
+        currValue++;
+        await updateDoc(questionRef, {[answerId]: currValue})
+        return currValue
+    } else {
+        console.log("Question doesn't exist");
+    }
+}
