d9b784936381488608b1c0f7904736330ae31fd5
Megan Weijiang <megan.weijiang@gmail.com>
2022-06-28 18:29:46 -0500

    add end page

---
+++ src/components/End.js
@@ -0,0 +1,20 @@
+import React from "react";
+import ImageContainer from "./ImageContainer"
+
+const End = ({data}) => {
+    const imageName = require(`../images/${data.image}`)
+    return (
+        <div className="wrapper">
+            <ImageContainer classname="center" image={imageName}/>
+            <div className="box-yellow-bg">
+                <p>{data.text}</p>
+                <p>You achieved {data.name} ending and earned the {data.badge} badge!</p>
+            </div>
+            <div className="centered-button-wrapper">
+                <button>See Results</button>
+            </div>
+        </div>
+    );
+}
+
+export default End
--- src/utils/utils.js
+++ src/utils/utils.js
@@ -27,3 +27,25 @@ export function recordChoice(questionId, choiceId) {
 
     incrementQuestionAnswerTally(`${questionId}`, `${choiceId}`)
 }
+
+export function getBadgesRecord(){
+    let badges = localStorage.getItem('badges');
+    if (badges) {
+	    return JSON.parse(badges);
+    } else {
+	    return null;
+    }
+}
+
+function setBadgesRecord(badges) {
+    localStorage.setItem('badges', JSON.stringify(badges));
+}
+
+export function recordBadge(badge) {
+    let badges = getBadgesRecord();
+    if (!badges) {
+        badges = []
+    }
+    badges.push(badge)
+    setBadgesRecord(badges)
+}
