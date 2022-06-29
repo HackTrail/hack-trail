7b2c66d143fb11063006ab084411df0a1871a421
Michael Gardo <michael.gardo@opcity.com>
2022-06-28 20:58:21 -0500

    First draft

--- src/components/Badges.js
+++ src/components/Badges.js
@@ -1,9 +1,17 @@
 import React from "react"
+import {getBadgesRecord} from "../utils/utils";
 
 const Badges = ({props}) => {
     return (
-        <div>
-            Badges
+        <div className={"badgeDisplayCase"}>
+            <p>Badges Achieved So Far</p>
+            <div class="badgeRow">
+                {
+                    getBadgesRecord().map((badge) => {
+                        return (<div className="badge" key={uuidv4()} id={badge}>`${badge}`</div>)
+                    })
+                }
+            </div>
         </div>
     )
 }
