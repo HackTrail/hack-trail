bcc7e8e5226f580359a445c58851cdbe470b095e
Juan Raigosa <juan.raigosa@opc-mbp-jraigosa.attlocal.net>
2022-06-28 21:27:53 -0500

    try get tally

--- src/components/Results.js
+++ src/components/Results.js
@@ -4,9 +4,8 @@ import getQuestionAnswerTally from "../db/firestore"
 
 
 const Results = ({data}) => {
-    const getQuestionAnsTally = (e) => {
-        const tally = getQuestionAnswerTally(1)
-    }
+
+    const tally = getQuestionAnswerTally(1)
 
     return (
         <div>
