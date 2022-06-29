fe8c5212424f79efaedf8b0c31d25355e13da9b4
Megan Weijiang <megan.weijiang@gmail.com>
2022-06-29 15:51:03 -0500

    don't fail on linting

--- src/components/Results.js
+++ src/components/Results.js
@@ -52,6 +52,7 @@ const Results = ({questions, resetGame}) => {
           setMappings(data);
           console.log(mappings)
         })();
+        // eslint-disable-next-line react-hooks/exhaustive-deps
       }, []);
 
     return (
