a6140593acac65e5cc24ba072a4c3d9d1408a393
Megan Weijiang <megan.weijiang@gmail.com>
2022-06-29 08:48:34 -0500

    styling adjustments

--- src/components/QuestionPrompt.js
+++ src/components/QuestionPrompt.js
@@ -1,13 +1,12 @@
 import React from "react";
 import Question from './Question';
 import ImageContainer from './ImageContainer'
-import './QuestionPrompt.css';
 
 const QuestionPrompt = (props) => {
     const imageName = require(`../images/${props.image}`)
     return (
-        <div className="questionPrompt wrapper">
-            <ImageContainer className="questionImages" image={imageName}/>
+        <div className="wrapper">
+            <ImageContainer image={imageName}/>
             <Question id={props.id} nextInfo={props.nextInfo} updateScores={props.updateScores} question={props.text} options={props.options}/>
         </div>
     );
