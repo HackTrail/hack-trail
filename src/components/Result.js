8f0d0f15b14fba00031dd9b9ac7007005d221065
Juan Raigosa <juan.raigosa@opc-mbp-jraigosa.attlocal.net>
2022-06-28 19:16:26 -0500

    Add badges comp, css for Result page

--- src/components/Result.js
+++ src/components/Result.js
@@ -1,9 +1,15 @@
 import React from "react"
+import Badges from "./Badges"
+import getQuestionAnswerTally from "../db/firestore"
 
 const Result = ({props}) => {
     return (
         <div>
+            <div>
+
+            </div>
             Result
+            <Badges props={props}/>
         </div>
     )
 }
